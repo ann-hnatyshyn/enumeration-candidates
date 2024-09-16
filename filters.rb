c41959a27ccdd60a1017d6ccdd8f478428e4aa79
Ann <a.m.hnatyshyn@gmail.com>
2024-09-16 14:51:17 -0300

    define filters

--- filters.rb
+++ filters.rb
@@ -2,15 +2,29 @@
 # This way, we keep these methods separated from other potential parts of the program
 
 def find(id)
-    # Your code Here
-  end
+  if id === candidate[:id]
+    print candidate
+  else 
+    print nil
+ end
   
   def experienced?(candidate)
-    # Your code Here
+    if candidate[:years_of_experience] >= 2
+      print true
+    else
+      print false
   end
   
   def qualified_candidates(candidates)
-    # Your code Here
+    print candidate if
+    candidate[:years_of_experience] >= 2
+    candidate[:github_points] >= 100
+    candidate[:languages] == 'Ruby' || 'Python'
+    candidate[:date_applied] 
   end
   
   # More methods will go below
+
+  def ordered_by_qualifications (candidates)
+    candidates.sort_by{|candidate| [:years_of_experience], [:github_points]}
+  end
